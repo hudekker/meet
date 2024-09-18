959491e8b7a765cd8fca4d8f5c451cbe89741a87
Robbie Hudekker <hudekker@gmail.com>
2024-09-18 16:29:43 +0800

    1

---
+++ meet-v20.2/background/bgEvents.js
@@ -0,0 +1,154 @@
+// Multiple windows
+// chrome.windows.onFocusChanged.addListener((windowId) => {
+//   setContextMenu();
+//   return true;
+// });
+
+// chrome.tabs.onActivated.addListener((tabId, changeInfo, tab) => {
+//   setContextMenu();
+//   return true;
+// });
+
+// chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
+
+chrome.runtime.onMessage.addListener((payload, sender, callback) => {
+  (async () => {
+    try {
+      let myTabs;
+      let tabs;
+
+      switch (payload.action) {
+        // Currently not used
+        case "GETSPKSTATE":
+          // Get the current muted state
+          tabsQuery = await chromeTabsQuery2({ url: sender.tab.url });
+          boolMuted = tabsQuery[0].mutedInfo.muted;
+
+          // Return the current state
+          callback({ boolMuted: boolMuted });
+          break;
+
+        // From content
+        case "SPKBTNCLICKED":
+          {
+            // Get the current muted state
+
+            let tabId = sender.tab.id;
+            let tab = await chromeTabsGet(tabId);
+            let boolMuted = !tab.mutedInfo.muted;
+
+            // Test it out 09/14/2024
+            // boolMuted = sender.mutedInfo.muted;
+
+            console.log(`SPKBTNCLICKED for tabId ${tabId} from ${tab.mutedInfo.muted} to ${boolMuted}`, tab);
+
+            chrome.tabs.update(tabId, { muted: boolMuted }, callback({ boolMuted }));
+          }
+          return true;
+          break;
+
+        case "tellContextToUpdateTabs":
+          let tabs = await chromeTabsQuery({});
+
+          let tabs2 = tabs.filter((el) => {
+            let bool = false;
+
+            if (el.url && el.url.startsWith("https://meet.google.com/")) {
+              bool = true;
+            }
+
+            return bool;
+          });
+
+          for (let i = 0; i < tabs2.length; i++) {
+            console.log(`tab is is ${tabs2[i].id}, url is ${tabs2[i].url}`);
+            // await chromeTabsSendMessage(tabs2[i].id, {
+            //   action: "updateTabTitle",
+            // });
+            chrome.tabs.sendMessage(tabs2[i].id, {
+              action: "updateTabTitle",
+            });
+          }
+          break;
+
+        // Todo: look at this because gt_ids can be out of date
+        case "getTableOfTabs":
+          callback(gt_ids);
+          break;
+
+        case "getAvailWidthHeight":
+          callback({ width: g_availWidth, height: g_availHeight });
+          break;
+
+        case "closeRoomTab":
+          mySender = sender;
+          tabId = payload.tabId;
+          winId = payload.winId;
+
+          tabId = sender.tab.id;
+          winId = sender.tab.windowId;
+          if (Number.isInteger(tabId)) {
+            chrome.tabs.remove(tabId);
+          }
+          break;
+
+        case "closeRoomTab2":
+          tabId = payload.tabId;
+
+          if (Number.isInteger(tabId)) {
+            chrome.tabs.remove(tabId);
+          }
+          break;
+
+        case "openWinMulti":
+          await openWinMulti(payload);
+          await sleep(1000);
+          await chromeRuntimeSendMessage({
+            action: "updateSliderFocus",
+          });
+          callback({ msg: "Opened Windows" });
+          break;
+
+        case "openTabMulti":
+          await openTabMulti(payload);
+          await sleep(1000);
+          await chromeRuntimeSendMessage({
+            action: "updateSliderFocus",
+          });
+          callback({ msg: "Opened Tabs" });
+          break;
+
+        case "getTabMuteStatus":
+          try {
+            // Get the tab ID from the sender
+            const tabId = sender.tab.id;
+
+            // Get the full tab info, including the mute status
+            const tabInfo = await new Promise((resolve, reject) => {
+              chrome.tabs.get(tabId, (tab) => {
+                if (chrome.runtime.lastError) {
+                  return reject(chrome.runtime.lastError);
+                }
+                resolve(tab);
+              });
+            });
+
+            // Send back the full tab info as the response
+            callback(tabInfo);
+          } catch (error) {
+            console.error("Error retrieving tab info:", error);
+            callback({ error: "Failed to retrieve tab info" });
+          }
+
+          // Return true to indicate we will send a response asynchronously
+          return true;
+          break;
+
+        default:
+          break;
+      }
+    } catch (err) {}
+  })();
+
+  return true;
+});
